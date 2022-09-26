f0da7aff05fa2d0c0b1e0b7006aa0ecf45df22fb
muhyusriln <yusrilnugrahaputra@gmail.com>
2022-09-26 21:22:59 +0700

    latihan Java Script Functional Programming

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ document.getElementById('dataCar').innerHTML = cars;
 const car = {
   type: "Fiat",
   model: "500",
+  weight: "850kg",
   color: "White"
 };
 
@@ -29,3 +30,34 @@ car.color = "Blue";
 car.owner = "Yusril"
 // display the property
 document.getElementById('objectArray').innerHTML = "Car owner is " + car.owner;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+document.getElementById('multipleNumber').innerHTML = myFunction(10, 20);
+
+// object created with advanced step
+function Person(firstName, lastName, age, status) {
+  // property
+  this.firstName = firstName,
+  this.lastName = lastName,
+  this.age = age,
+  this.status = status
+
+  // method
+  this.fullName = function() {
+    return `${this.firstName} ${this.lastName}`
+  }
+
+  this.identityPerson = function() {
+    return `${this.firstName} ${this.lastName} is 
+    ${this.age} years old, status as a ${this.status}`;
+    // return this.fullName + " is " + this.age + " years old " + " status as a " + this.status;
+  }
+  this.showName = function() {
+    document.getElementById('person').innerHTML = this.identityPerson();
+  }
+}
+
+const person1 = new Person("Muhammad", "Yusril Nugraha", 23, "freshgraduate");
+person1.showName();
