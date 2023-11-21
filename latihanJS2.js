ee1dafca439526ca7192d08f8b40e21017798062
muhyusriln <yusrilnugrahaputra@gmail.com>
2023-11-22 08:34:35 +0900

    learning JavaScript day-8

---
+++ latihanJS2.js
@@ -0,0 +1,135 @@
+/* =========================== Function =========================== */
+function multiply(a, b) {
+  return a * b;         // function dapat mengembalikan suatu nilai
+}
+
+let result = multiply(4, 6);
+console.log(result);
+
+/*  tanpa pengembalian nilai. langsung mengeluarkan nilai
+function greeting(language, name) {
+  if(language === "English") {
+    console.log(`Good Morning ${name}`);
+  } else if(language === "French") {
+    console.log(`Bonjour ${name}`);
+  } else if(language === "Japanese") {
+    console.log(`Ohayo ${name}`);
+  } else {
+    console.log(`Selamat Pagi ${name}`);
+  }
+}
+
+greeting("Japanese", "Yusril");
+*/
+
+// Declaration Function
+function greeting (language, name) {
+  if(language === "English") {
+    return `Good Morning ${name}`;
+  } else if(language === "French") {
+    return `Bonjour ${name}`;
+  } else if(language === "Japanese") {
+    return `Ohayo ${name}`;
+  } else {
+    return `Selamat Pagi ${name}`;
+  }
+}
+
+let greetingMessage = greeting("French", "Yusril");
+console.log(greetingMessage);
+
+/* Expression Function
+function greeting(name, language) {
+  if(language === "English") {
+    return `Good Morning ${name}!`
+  } else if (language === "French") {
+    return `Bonjour ${name}!`;
+  } else {
+    return `Selamat Pagi ${name}!`;
+  }
+}
+
+let greetingMessage = greeting("Harry", "French");
+console.log(greetingMessage);
+*/
+
+// function juga dapat dimanfaatkan sebagai destructuring object
+const user = {
+  id: 13,
+  displayName: 'yusril',
+  fullName: 'Muhammad Yusril Nugraha Putra'
+}
+
+function introduction({ displayName, fullName}) {
+  console.log(`${displayName} is ${fullName}`);
+}
+
+introduction(user);
+console.log("End of Declaring Function and Expression Function");
+
+/* ================= Default Parameter ================= */
+// code syntax Default Paramter
+function exponentsFormula(baseNumber, exponent = 2) {     // bisa memberikan value default di parameter yang tidak didefinisikan
+  const result = baseNumber ** exponent;
+  console.log(`${baseNumber}^${exponent} = ${result}`);
+}
+
+exponentsFormula(7);
+console.log("End of Default Parameter");
+
+/* ================= Rest Parameter ================= */
+// code syntax Default Paramter
+
+function sum(...numbers) {
+  let result = 0;
+  for (let number of numbers) {
+    result += number;
+  }
+  return result;
+}
+
+console.log(sum(1, 2, 3, 4, 5));
+console.log("End of Rest Parameter");
+
+/* ================= Arrow Function ================= */
+// code syntax Arrow Function
+
+const sayName = name => {                 // jika fungsi hanya memiliki satu parameter
+  console.log(`Name saya ${name}`);
+}
+sayName("Leia");
+
+const sayHello = () => {                  // jika fungsi sama sekali tidak membutuhkan parameter
+  console.log("Selamat pagi semuanya");
+}
+sayHello();
+
+const sayNameOneLine = name => console.log(`My Name is ${name}`);     // ketika body dari function hanya terdiri dari satu baris
+sayNameOneLine("Nadira Veronica Anidya");
+
+const sayHelloOneLine = () => console.log("Selamat pagi semuanya!");     // ketika body dari function hanya terdiri dari satu baris
+sayHelloOneLine();
+
+const exponentsFormulaOne = (a, b) => a ** b;
+console.log(exponentsFormulaOne(3, 3));
+
+/* ================= Rest Parameter ================= */
+// code syntax Default Paramter
+/* ================= Rest Parameter ================= */
+// code syntax Default Paramter
+/* ================= Rest Parameter ================= */
+// code syntax Default Paramter
+/* ================= Rest Parameter ================= */
+// code syntax Default Paramter
+/* ================= Rest Parameter ================= */
+// code syntax Default Paramter
+/* ================= Rest Parameter ================= */
+// code syntax Default Paramter
+/* ================= Rest Parameter ================= */
+// code syntax Default Paramter
+
+
+
+
+
+
