213f335e5824b4784865a8df0c9e35f0e88bf228
muhyusriln <yusrilnugrahaputra@gmail.com>
2023-12-15 10:40:11 +0900

    Learning JavaScript day-19

---
+++ latihanJS5.js
@@ -0,0 +1,86 @@
+/* =========================== Penanganan Error =========================== */
+// code syntax penanganan error
+try {
+  console.log("Awal block try");
+  // errorCode;        // jika terjadi error
+  console.log("Akhir block try");
+}
+catch(error) {
+  console.log("Terjadi error");
+  // cara penangan error dengan menggunakan object error
+  console.log(error.name);
+  console.log(error.message);
+  console.log(error.stack);
+} finally {
+  console.log("Akan tetap dieksekusi");
+}
+
+/* =========================== Throwing Errors =========================== */
+// code syntax used throwing error
+const json = '{"name": "John Doe", "age": 25 }';          // parsing dari variabel json (string) menjadi sebuah object
+// const json = '{ "age": 25 }';          // contoh variabel name tidak diinisiasikan
+// const json = '{ bad json }';       // contoh wrong code syntax
+
+try {
+  const user = JSON.parse(json);
+
+  if(!user.name) {
+    throw new SyntaxError("'name' is required.");
+  }
+  // errorCode;
+
+  console.log(user.name);
+  console.log(user.age);
+} catch(error) {
+  // Dengan operator instanceOf, kita bisa mendapatkan tipe dari eror yang terjadi. 
+  //Dari sana kita bisa membuat percabangan bagaimana cara menangani erornya.
+  if(error instanceof SyntaxError) {
+    console.log(`JSON Error: ${error.message}`);  
+  } else if(error instanceof ReferenceError) {
+    console.log(error.message);
+  } else {
+    console.log(error.stack);
+  }
+
+  // console.log(error.name);
+  // console.log(error.message);
+  // console.log(`JSON Error: ${error.message}`);
+}
+
+
+/* =========================== Custom Error =========================== */
+// code syntax used class validation
+class validationError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "validationError";
+  }
+}
+
+// const json1 = '{"merk": "iphone", "type": 15, "realease_year": 2023 }';
+const json1 = '{ "type": 15, "realease_year": 2023 }';
+
+try {
+  const smartphone = JSON.parse(json1);
+
+  if(!smartphone.merk) {
+    throw new validationError("'merk' is required");
+  } 
+  if(!smartphone.type) {
+    throw new validationError("'type' is required") ;
+  }
+
+  console.log(smartphone.merk);
+  console.log(smartphone.type);
+  
+} catch(error) {
+  if(error instanceof SyntaxError) {
+    console.log(`JSON syntax error: ${error.message}`);
+  } else if(error instanceof validationError) {
+    console.log(`Invalid data: ${error.message}`);
+  } else if(error instanceof ReferenceError) {
+    console.log(error.message);
+  } else {
+    console.log(error.stack);
+  }
+}
--- quiz2.js
+++ quiz2.js
@@ -75,3 +75,100 @@ const greatAuthors = books.filter((book) => book.sales > 1000000).map((book) => 
 
 console.log(greatAuthors);
 
+/**
+ * Saat ini, Anda sudah memiliki fungsi detectTriangle yang berguna untuk
+ * mendeteksi jenis segitiga berdasarkan nilai argumen.
+ * Contoh:
+ *  - 1, 1, 1 -> Segitiga sama sisi
+ *  - 4, 4, 2 -> Segitiga sama kaki
+ *  - 3, 4, 6 -> Segitiga sembarang
+ *
+ * Namun fungsi detectTriangle belum berjalan dengan baik karena
+ * bila ada argumen fungsi yang bukan number, alih-alih error, ia akan mengembalikan "Segitiga sembarang".
+ * Contoh:
+ *  - 1, false, 1 -> Segitiga sembarang
+ *  - 'a', 3, 5 -> Segitiga sembarang
+ *  - 12, 2, null -> Segitiga sembarang
+ * Kondisi yang diharapkan:
+ *  - 1, false, 1 -> Argumen kedua harus number
+ *  - 'a', 3, 5 -> Argumen pertama harus number
+ *  - 12, 2, null -> Argumen ketiga harus number
+ *
+ *  Tugas Anda adalah memperbaiki fungsi detectTriangle agar berjalan dengan kondisi yang diharapkan.
+ *  Pastikan Anda menggunakan teknik Throwing dan Handling Error yah.
+ *
+ * TODO 1:
+ * - Buatlah class ValidationError yang merupakan custom error dengan spesifikasi berikut:
+ *   - Turunan dari class Error
+ *   - Memiliki constructor(message)
+ *   - this.name harus bernilai "ValidationError"
+ *
+ * TODO 2:
+ * - Buatlah fungsi validateNumberInput yang memvalidasi 3 buah input (argumen) dengan spesifikasi berikut:
+ *   - Menerima 3 argumen
+ *   - Bila argumen pertama bukan number:
+ *     - throw ValidationError dengan pesan 'Argumen pertama harus number'
+ *   - Bila argumen kedua bukan number:
+ *     - throw ValidationError dengan pesan 'Argumen kedua harus number'
+ *   - Bila argumen ketiga bukan number:
+ *     - throw ValidationError dengan pesan 'Argumen ketiga harus number'
+ *
+ * TODO 3:
+ * - Panggil fungsi validateNumberInput di dalam fungsi detectTriangle untuk memvalidasi nilai argumen a, b, dan c.
+ *   - pastikan Anda memanggil validateNumberInput menggunakan try .. catch.
+ *   - bila block catch terpanggil, kembalikan fungsi detectTriangle dengan pesan error yang dibawa fungsi validateNumberInput.
+ */
+let x = 1;
+console.log(typeof x);
+let y = "yusril";
+console.log(typeof y);
+let z = "number";
+console.log(typeof z);
+
+if(x !== z) {
+  console.log("benar, ini tidak sesuai value dan tipe datanya");
+} else {
+  console.log("salah, ini sesuai value dan tipe datanya");
+
+}
+
+// TODO 1
+class validationError extends Error {
+	constructor(message) {
+	super(message);
+    this.name = "validationError";
+	}
+}
+
+// TODO 2
+const validateNumberInput = (a, b, c) => {
+  if(typeof a !== "number") {
+  throw new validationError("Argumen pertama harus number");
+  } else if(typeof b !== "number") {
+    throw new validationError("Argumen kedua harus number");
+  } else if(typeof c !== "number") {
+    throw new validationError("Argumen ketiga harus number");
+  }
+}
+
+
+const detectTriangle = (a, b, c) => {
+  // TODO 3
+  try {
+    validateNumberInput(a, b, c);
+  }catch(error) {
+    if(error instanceof validationError) {
+      console.log(error.message);
+    }
+  }
+  if (a === b && b === c) {
+    return 'Segitiga sama sisi';
+  }
+
+  if (a === b || a === c || b === c) {
+    return 'Segitiga sama kaki';
+  }
+
+  return 'Segitiga sembarang';
+};
+console.log(detectTriangle(2, 3, 7));
